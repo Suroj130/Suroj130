721e5440a1606a622e7d7619c915ea6a558c57a7
Suroj130 <sandipbera.ind@gmail.com>
2025-04-10 18:34:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/antbotsX")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Bots_Apdate")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FriendZone_Ever")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
